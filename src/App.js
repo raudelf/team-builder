3d12ab947f2e0b3e622508fd55d20fbf1e72f589
Raudel Flores <rawdell@outlook.com>
2019-12-10 20:15:42 -0600

    installed from create react app and cleared the page to start on project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [ members, setMembers ] = useState('');
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
