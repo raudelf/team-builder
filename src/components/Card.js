655a0a0c20927e635dbd2867a6ae71610dafdf77
Raudel Flores <rawdell@outlook.com>
2019-12-10 21:40:39 -0600

    created functions to generate form boxes and cards with info from form info

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Cards = (props) => {
+    return (
+        <div className='cards'>
+        {props.member.map(m => (
+            <div className='cardInfo' key={m.number}>
+            <h2>{m.member}</h2>
+            <p>{m.email}</p>
+            </div>
+        ))}
+        </div>
+    )
+}
+
+export default Cards;
---
+++ src/components/form.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Form = props => {
+
+
+return (
+    <form>
+        <label>Name</label>
+        <input 
+        type='text'
+        name='member'
+        placeholder='Full Name'
+        />
+    </form>
+)
+}
+
+export default Form;
